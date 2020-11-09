d3542c3adfb523eeb443828e164050d3b7bd9922
Philipp Schmid <philipp95.schmid@gmail.com>
2020-11-09 15:07:31 +0100

    added minimum macOS platform to CloudAccessPrivate

--- CloudAccessPrivate/Package.swift
+++ CloudAccessPrivate/Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
 	name: "CloudAccessPrivate",
 	platforms: [
-		.iOS(.v11)
+		.iOS(.v11),
+		.macOS(.v10_12)
 	],
 	products: [
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
