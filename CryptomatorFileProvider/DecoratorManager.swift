f0258f2587590342ed70cc66c3a525341d581735
Philipp Schmid <philipp95.schmid@gmail.com>
2020-11-30 15:05:16 +0100

    Added Semaphore to DecoratorManager to ensure that there is only one FileProviderDecorator per FileProviderDomainIdentifier

--- CryptomatorFileProvider/DecoratorManager.swift
+++ CryptomatorFileProvider/DecoratorManager.swift
@@ -10,12 +10,16 @@ import FileProvider
 import Foundation
 public class DecoratorManager {
 	private static var cachedDecorators = [NSFileProviderDomainIdentifier: FileProviderDecorator]()
+	private static let semaphore = DispatchSemaphore(value: 1)
+
 	public static func getDecorator(for domain: NSFileProviderDomain, with manager: NSFileProviderManager, dbPath: URL) throws -> FileProviderDecorator {
+		semaphore.wait()
 		if let cachedDecorator = cachedDecorators[domain.identifier] {
 			return cachedDecorator
 		}
 		let decorator = try FileProviderDecorator(for: domain, with: manager, dbPath: dbPath)
 		cachedDecorators[domain.identifier] = decorator
+		semaphore.signal()
 		return decorator
 	}
 }
