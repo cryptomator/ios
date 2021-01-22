88dfd0ee9f331ca0680d134a1fdc4f16b478f53d
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2021-01-22 12:26:09 +0100

    Removed HeaderView from VaultListViewController

--- Cryptomator/VaultList/VaultListViewController.swift
+++ Cryptomator/VaultList/VaultListViewController.swift
@@ -11,7 +11,7 @@ import Foundation
 import UIKit
 
 class VaultListViewController: UITableViewController {
-	private let header = HeaderView(title: "Vaults".uppercased(), editButtonTitle: "Edit")
+	private let header = TableViewHeader(title: "Vaults".uppercased(), editButtonTitle: "Edit")
 	private let viewModel: VaultListViewModelProtocol
 	weak var coordinator: MainCoordinator?
 
@@ -130,48 +130,6 @@ class VaultListViewController: UITableViewController {
 
 			present(alertController, animated: true, completion: nil)
 		}
-	}
-}
-
-private class HeaderView: UITableViewHeaderFooterView {
-	let editButton = UIButton(type: .system)
-	let title = UILabel()
-
-	convenience init(title: String, editButtonTitle: String) {
-		self.init()
-		self.title.text = title
-		editButton.setTitle(editButtonTitle, for: .normal)
-	}
-
-	convenience init() {
-		self.init(reuseIdentifier: nil)
-
-		editButton.setTitleColor(UIColor(named: "primary"), for: .normal)
-		editButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .footnote)
-
-		title.font = UIFont.preferredFont(forTextStyle: .footnote)
-		if #available(iOS 13, *) {
-			title.textColor = .secondaryLabel
-		} else {
-			title.textColor = UIColor(named: "secondaryLabel")
-		}
-
-		editButton.translatesAutoresizingMaskIntoConstraints = false
-		title.translatesAutoresizingMaskIntoConstraints = false
-
-		contentView.addSubview(editButton)
-		contentView.addSubview(title)
-
-		NSLayoutConstraint.activate([
-			title.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
-			title.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-			title.heightAnchor.constraint(equalTo: contentView.layoutMarginsGuide.heightAnchor),
-
-			editButton.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
-			editButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-			editButton.heightAnchor.constraint(equalTo: contentView.layoutMarginsGuide.heightAnchor)
-
-		])
 	}
 }
 
