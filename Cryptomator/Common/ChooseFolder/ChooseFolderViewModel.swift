cryptomator/ios

commit d06c7bf7a027a28cf2a815f18bad0613849b800d

Added support for vault format 8

Cryptomator/Common/ChooseFolder/ChooseFolderViewModel.swift
@@ -14,7 +14,7 @@ protocol ChooseFolderViewModelProtocol {
 	var cloudPath: CloudPath { get }
 	var foundMasterkey: Bool { get }
 	var items: [CloudItemMetadata] { get }
-	func startListenForChanges(onError: @escaping (Error) -> Void, onChange: @escaping () -> Void, onMasterkeyDetection: @escaping (CloudPath) -> Void)
+	func startListenForChanges(onError: @escaping (Error) -> Void, onChange: @escaping () -> Void, onVaultDetection: @escaping (Item) -> Void)
 	func refreshItems()
 }
 
@@ -33,7 +33,7 @@ class ChooseFolderViewModel: ChooseFolderViewModelProtocol {
 	private let provider: CloudProvider
 	private var errorListener: ((Error) -> Void)?
 	private var changeListener: (() -> Void)?
-	private var masterkeyListener: ((CloudPath) -> Void)?
+	private var vaultListener: ((Item) -> Void)?
 
 	init(canCreateFolder: Bool, cloudPath: CloudPath, provider: CloudProvider) {
 		self.canCreateFolder = canCreateFolder
@@ -41,19 +41,17 @@ class ChooseFolderViewModel: ChooseFolderViewModelProtocol {
 		self.provider = provider
 	}
 
-	func startListenForChanges(onError: @escaping (Error) -> Void, onChange: @escaping () -> Void, onMasterkeyDetection: @escaping (CloudPath) -> Void) {
+	func startListenForChanges(onError: @escaping (Error) -> Void, onChange: @escaping () -> Void, onVaultDetection: @escaping (Item) -> Void) {
 		errorListener = onError
 		changeListener = onChange
-		masterkeyListener = onMasterkeyDetection
+		vaultListener = onVaultDetection
 		refreshItems()
 	}
 
 	func refreshItems() {
 		provider.fetchItemListExhaustively(forFolderAt: cloudPath).then { itemList in
-			#warning("TODO: Consider Vault Format 8")
-			if let masterkeyItem = itemList.items.first(where: { $0.name == "masterkey.cryptomator" && $0.itemType == .file }), itemList.items.contains(where: { $0.name == "d" && $0.itemType == .folder }) {
-				self.foundMasterkey = true
-				self.masterkeyListener?(masterkeyItem.cloudPath)
+			if let vaultItem = self.getVaultItem(items: itemList.items) {
+				self.vaultListener?(vaultItem)
 			} else {
 				self.items = itemList.items
 				self.changeListener?()
@@ -61,5 +59,33 @@ class ChooseFolderViewModel: ChooseFolderViewModelProtocol {
 		}.catch { error in
 			self.errorListener?(error)
 		}
+	}
+
+	func getVaultItem(items: [CloudItemMetadata]) -> Item? {
+		if let vaultConfigPath = getVaultConfigCloudPath(items: items) {
+			return Item(type: .vaultConfig, path: vaultConfigPath)
+		} else if let legacyMasterkeyPath = getLegacyMasterkeyPath(items: items) {
+			return Item(type: .legacyMasterkey, path: legacyMasterkeyPath)
+		} else {
+			return nil
+		}
+	}
+
+	func getVaultConfigCloudPath(items: [CloudItemMetadata]) -> CloudPath? {
+		let vaultConfigItem = items.first(where: { $0.name == "vault.cryptomator" && $0.itemType == .file })
+		guard items.contains(where: { $0.name == "d" && $0.itemType == .folder }) else {
+			print("Missing d folder")
+			return nil
+		}
+		return vaultConfigItem?.cloudPath
+	}
+
+	func getLegacyMasterkeyPath(items: [CloudItemMetadata]) -> CloudPath? {
+		let masterkeyItem = items.first(where: { $0.name == "masterkey.cryptomator" && $0.itemType == .file })
+		guard items.contains(where: { $0.name == "d" && $0.itemType == .folder }) else {
+			print("Missing d folder")
+			return nil
+		}
+		return masterkeyItem?.cloudPath
 	}
 }

Cryptomator.xcodeproj/project.pbxproj
@@ -60,6 +60,7 @@
 		4A66F58225C487C9001BE15E /* PasswordFieldCell.swift in Sources */ = {isa = PBXBuildFile; fileRef = 4A66F58125C487C9001BE15E /* PasswordFieldCell.swift */; };
 		4A66F58B25C489C7001BE15E /* OpenExistingVaultPasswordViewModel.swift in Sources */ = {isa = PBXBuildFile; fileRef = 4A66F58A25C489C7001BE15E /* OpenExistingVaultPasswordViewModel.swift */; };
 		4A717CD924C835740048E08F /* ReparentTaskManagerTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 4A717CD824C835740048E08F /* ReparentTaskManagerTests.swift */; };
+		4A753DB92678A226005F79C1 /* OpenExistingLegacyVaultPasswordViewModel.swift in Sources */ = {isa = PBXBuildFile; fileRef = 4A753DB82678A226005F79C1 /* OpenExistingLegacyVaultPasswordViewModel.swift */; };
 		4A797F8F24AC6731007DDBE1 /* FileProviderItemTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 4A797F8E24AC6731007DDBE1 /* FileProviderItemTests.swift */; };
 		4A797F9624AC9936007DDBE1 /* CloudProviderMock.swift in Sources */ = {isa = PBXBuildFile; fileRef = 4A797F9524AC9936007DDBE1 /* CloudProviderMock.swift */; };
 		4A797F9824AC9A1B007DDBE1 /* CloudProviderMockTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 4A797F9724AC9A1B007DDBE1 /* CloudProviderMockTests.swift */; };
@@ -316,6 +317,7 @@
 		4A66F58125C487C9001BE15E /* PasswordFieldCell.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PasswordFieldCell.swift; sourceTree = "<group>"; };
 		4A66F58A25C489C7001BE15E /* OpenExistingVaultPasswordViewModel.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = OpenExistingVaultPasswordViewModel.swift; sourceTree = "<group>"; };
 		4A717CD824C835740048E08F /* ReparentTaskManagerTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ReparentTaskManagerTests.swift; sourceTree = "<group>"; };
+		4A753DB82678A226005F79C1 /* OpenExistingLegacyVaultPasswordViewModel.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = OpenExistingLegacyVaultPasswordViewModel.swift; sourceTree = "<group>"; };
 		4A797F8E24AC6731007DDBE1 /* FileProviderItemTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = FileProviderItemTests.swift; sourceTree = "<group>"; };
 		4A797F9524AC9936007DDBE1 /* CloudProviderMock.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = CloudProviderMock.swift; sourceTree = "<group>"; };
 		4A797F9724AC9A1B007DDBE1 /* CloudProviderMockTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = CloudProviderMockTests.swift; sourceTree = "<group>"; };
@@ -788,6 +790,7 @@
 				4A2FD08A25B5E437008565C8 /* OpenExistingVaultCoordinator.swift */,
 				4AA8615025C1DB5E002A59F5 /* OpenExistingVaultPasswordViewController.swift */,
 				4A66F58A25C489C7001BE15E /* OpenExistingVaultPasswordViewModel.swift */,
+				4A753DB82678A226005F79C1 /* OpenExistingLegacyVaultPasswordViewModel.swift */,
 			);
 			path = OpenExistingVault;
 			sourceTree = "<group>";
@@ -1409,6 +1412,7 @@
 				4AFCE51F25B89CD80069C4FC /* CloudProviderType+Localization.swift in Sources */,
 				4A447E0425BF0B0F00D9520D /* SingleSectionTableViewController.swift in Sources */,
 				4A9D1237261DAC5D00A670E2 /* WebDAVAuthenticationViewModel.swift in Sources */,
+				4A753DB92678A226005F79C1 /* OpenExistingLegacyVaultPasswordViewModel.swift in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};

Cryptomator/AddVault/OpenExistingVault/OpenExistingLegacyVaultPasswordViewModel.swift
@@ -1,0 +1,47 @@
+//
+//  OpenExistingLegacyVaultPasswordViewModel.swift
+//  Cryptomator
+//
+//  Created by Philipp Schmid on 15.06.21.
+//  Copyright © 2021 Skymatic GmbH. All rights reserved.
+//
+
+import CryptomatorCloudAccessCore
+import CryptomatorCommonCore
+import Foundation
+import Promises
+class OpenExistingLegacyVaultPasswordViewModel: OpenExistingVaultPasswordViewModelProtocol {
+	var password: String?
+	let provider: CloudProvider
+	let account: CloudProviderAccount
+
+	// later: localMasterkeyURL: URL instead of masterkeyPath: CloudPath
+	let masterkeyPath: CloudPath
+	var vaultName: String {
+		let masterkeyParentPath = masterkeyPath.deletingLastPathComponent()
+		return masterkeyParentPath.lastPathComponent
+	}
+
+	var footerTitle: String {
+		return String(format: NSLocalizedString("addVault.openExistingVault.password.footer", comment: ""), vaultName)
+	}
+
+	private let localMasterkeyURL: URL
+	let vaultUID: String
+
+	init(provider: CloudProvider, account: CloudProviderAccount, masterkeyPath: CloudPath, vaultID: String) {
+		self.provider = provider
+		self.account = account
+		self.masterkeyPath = masterkeyPath
+		let tmpDirURL = FileManager.default.temporaryDirectory
+		self.localMasterkeyURL = tmpDirURL.appendingPathComponent(UUID().uuidString, isDirectory: false)
+		self.vaultUID = vaultID
+	}
+
+	func addVault() -> Promise<Void> {
+		guard let password = password else {
+			return Promise(MasterkeyProcessingViewModelError.noPasswordSet)
+		}
+		return VaultManager.shared.createLegacyFromExisting(withVaultUID: vaultUID, delegateAccountUID: account.accountUID, masterkeyPath: masterkeyPath, password: password, storePasswordInKeychain: true)
+	}
+}

Cryptomator/AddVault/OpenExistingVault/OpenExistingVaultChooseFolderViewController.swift
@@ -10,16 +10,16 @@ import CryptomatorCloudAccessCore
 import UIKit
 
 class OpenExistingVaultChooseFolderViewController: ChooseFolderViewController {
-	private var masterkeyPath: CloudPath?
+	private var vault: Item?
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		title = NSLocalizedString("addVault.openExistingVault.title", comment: "")
 	}
 
-	override func showDetectedMasterkey(at path: CloudPath) {
-		masterkeyPath = path
-		let successView = SuccessView(viewModel: DetectedMasterkeyViewModel(masterkeyPath: path))
+	override func showDetectedVault(_ vault: Item) {
+		self.vault = vault
+		let successView = SuccessView(viewModel: DetectedMasterkeyViewModel(masterkeyPath: vault.path))
 		let containerView = UIView()
 		successView.translatesAutoresizingMaskIntoConstraints = false
 		containerView.addSubview(successView)
@@ -41,10 +41,10 @@ class OpenExistingVaultChooseFolderViewController: ChooseFolderViewController {
 	}
 
 	@objc func addVault() {
-		guard let masterkeyPath = masterkeyPath else {
+		guard let vault = vault else {
 			return
 		}
-		coordinator?.chooseItem(at: masterkeyPath)
+		coordinator?.chooseItem(vault)
 	}
 }
 

Cryptomator/AddVault/OpenExistingVault/OpenExistingVaultCoordinator.swift
@@ -94,8 +94,17 @@ private class AuthenticatedOpenExistingVaultCoordinator: VaultInstallationCoordi
 		parentCoordinator?.close()
 	}
 
-	func chooseItem(at path: CloudPath) {
-		let viewModel = OpenExistingVaultPasswordViewModel(provider: provider, account: account, masterkeyPath: path, vaultID: UUID().uuidString)
+	func chooseItem(_ item: Item) {
+		let viewModel: OpenExistingVaultPasswordViewModelProtocol
+		switch item.type {
+		case .vaultConfig:
+			viewModel = OpenExistingVaultPasswordViewModel(provider: provider, account: account, vaultConfigPath: item.path, vaultUID: UUID().uuidString)
+		case .legacyMasterkey:
+			viewModel = OpenExistingLegacyVaultPasswordViewModel(provider: provider, account: account, masterkeyPath: item.path, vaultID: UUID().uuidString)
+		default:
+			handleError(ExistingVaultCoordinatorError.wrongItemType, for: navigationController)
+			return
+		}
 		let passwordVC = OpenExistingVaultPasswordViewController(viewModel: viewModel)
 		passwordVC.coordinator = self
 		navigationController.pushViewController(passwordVC, animated: true)
@@ -142,4 +151,8 @@ private class AuthenticatedOpenExistingVaultCoordinator: VaultInstallationCoordi
 		comps?.scheme = "shareddocuments"
 		return comps?.url
 	}
+}
+
+enum ExistingVaultCoordinatorError: Error {
+	case wrongItemType
 }

Cryptomator/AddVault/OpenExistingVault/OpenExistingVaultPasswordViewModel.swift
@@ -26,34 +26,30 @@ class OpenExistingVaultPasswordViewModel: OpenExistingVaultPasswordViewModelProt
 	let account: CloudProviderAccount
 
 	// later: localMasterkeyURL: URL instead of masterkeyPath: CloudPath
-	let masterkeyPath: CloudPath
+	let vaultConfigPath: CloudPath
 	var vaultName: String {
-		let masterkeyParentPath = masterkeyPath.deletingLastPathComponent()
-		return masterkeyParentPath.lastPathComponent
+		let vaultConfigParentPath = vaultConfigPath.deletingLastPathComponent()
+		return vaultConfigParentPath.lastPathComponent
 	}
 
 	var footerTitle: String {
 		return String(format: NSLocalizedString("addVault.openExistingVault.password.footer", comment: ""), vaultName)
 	}
 
-	private let localMasterkeyURL: URL
 	let vaultUID: String
 
-	init(provider: CloudProvider, account: CloudProviderAccount, masterkeyPath: CloudPath, vaultID: String) {
+	init(provider: CloudProvider, account: CloudProviderAccount, vaultConfigPath: CloudPath, vaultUID: String) {
 		self.provider = provider
 		self.account = account
-		self.masterkeyPath = masterkeyPath
-		let tmpDirURL = FileManager.default.temporaryDirectory
-		self.localMasterkeyURL = tmpDirURL.appendingPathComponent(UUID().uuidString, isDirectory: false)
-		self.vaultUID = vaultID
+		self.vaultConfigPath = vaultConfigPath
+		self.vaultUID = vaultUID
 	}
 
 	func addVault() -> Promise<Void> {
-		#warning("TODO: Remove Async Implementation")
 		guard let password = password else {
 			return Promise(MasterkeyProcessingViewModelError.noPasswordSet)
 		}
-		return VaultManager.shared.createFromExisting(withVaultID: vaultUID, delegateAccountUID: account.accountUID, masterkeyPath: masterkeyPath, password: password, storePasswordInKeychain: true)
+		return VaultManager.shared.createFromExisting(withVaultUID: vaultUID, delegateAccountUID: account.accountUID, vaultConfigPath: vaultConfigPath, password: password, storePasswordInKeychain: true)
 	}
 }
 

Cryptomator/Common/ChooseFolder/ChooseFolderViewController.swift
@@ -43,10 +43,10 @@ class ChooseFolderViewController: SingleSectionTableViewController {
 			guard let self = self else { return }
 			self.refreshControl?.endRefreshing()
 			self.tableView.reloadData()
-		} onMasterkeyDetection: { [weak self] masterkeyPath in
+		} onVaultDetection: { [weak self] vault in
 			guard let self = self else { return }
 			self.tableView.reloadData()
-			self.showDetectedMasterkey(at: masterkeyPath)
+			self.showDetectedVault(vault)
 		}
 	}
 
@@ -64,7 +64,7 @@ class ChooseFolderViewController: SingleSectionTableViewController {
 		}
 	}
 
-	func showDetectedMasterkey(at path: CloudPath) {
+	func showDetectedVault(_ vault: Item) {
 		fatalError("not implemented")
 	}
 
@@ -180,7 +180,7 @@ private class ChooseFolderViewModelMock: ChooseFolderViewModelProtocol {
 		self.cloudPath = cloudPath
 	}
 
-	func startListenForChanges(onError: @escaping (Error) -> Void, onChange: @escaping () -> Void, onMasterkeyDetection: @escaping (CloudPath) -> Void) {
+	func startListenForChanges(onError: @escaping (Error) -> Void, onChange: @escaping () -> Void, onVaultDetection: @escaping (Item) -> Void) {
 		onChange()
 	}
 

Cryptomator/Common/ChooseFolder/FolderChoosing.swift
@@ -11,6 +11,17 @@ import CryptomatorCloudAccessCore
 protocol FolderChoosing: AnyObject {
 	func showItems(for path: CloudPath)
 	func close()
-	func chooseItem(at path: CloudPath)
+	func chooseItem(_ item: Item)
 	func showCreateNewFolder(parentPath: CloudPath)
+}
+
+struct Item {
+	let type: ItemType
+	let path: CloudPath
+}
+
+enum ItemType {
+	case folder
+	case vaultConfig
+	case legacyMasterkey
 }

commit c77d507324ab6e43bd9c5667e8fdbaa9a430f75c

Fixed bug introduced in d06c7bf7a027a28cf2a815f18bad0613849b800d which prevented the ChooseFolderViewController header from being removed when a vault was found

Cryptomator/Common/ChooseFolder/ChooseFolderViewModel.swift
@@ -51,8 +51,10 @@ class ChooseFolderViewModel: ChooseFolderViewModelProtocol {
 	func refreshItems() {
 		provider.fetchItemListExhaustively(forFolderAt: cloudPath).then { itemList in
 			if let vaultItem = self.getVaultItem(items: itemList.items) {
+				self.foundMasterkey = true
 				self.vaultListener?(vaultItem)
 			} else {
+				self.foundMasterkey = false
 				self.items = itemList.items
 				self.changeListener?()
 			}