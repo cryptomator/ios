bc078dbb18d7fad662b5e058f3c1f9c590157763
Tobias Hagemann <tobias.hagemann@skymatic.de>
2021-08-19 13:31:06 +0200

    Added more localization for URLSessionError

--- CryptomatorCommon/Sources/CryptomatorCommonCore/URLSessionError+Localization.swift
+++ CryptomatorCommon/Sources/CryptomatorCommonCore/URLSessionError+Localization.swift
@@ -13,7 +13,22 @@ extension URLSessionError: LocalizedError {
 	public var errorDescription: String? {
 		switch self {
 		case let .httpError(_, statusCode: statusCode):
-			return String(format: LocalizedString.getValue("urlSession.error.httpError"), statusCode)
+			switch statusCode {
+			case 401:
+				return String(format: LocalizedString.getValue("urlSession.error.httpError.401"), statusCode)
+			case 403:
+				return String(format: LocalizedString.getValue("urlSession.error.httpError.403"), statusCode)
+			case 404:
+				return String(format: LocalizedString.getValue("urlSession.error.httpError.404"), statusCode)
+			case 405:
+				return String(format: LocalizedString.getValue("urlSession.error.httpError.405"), statusCode)
+			case 409:
+				return String(format: LocalizedString.getValue("urlSession.error.httpError.409"), statusCode)
+			case 412:
+				return String(format: LocalizedString.getValue("urlSession.error.httpError.412"), statusCode)
+			default:
+				return String(format: LocalizedString.getValue("urlSession.error.httpError.default"), statusCode)
+			}
 		case .unexpectedResponse:
 			return LocalizedString.getValue("urlSession.error.unexpectedResponse")
 		}
