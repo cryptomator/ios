41f463d43a9d451417344251d9f3a7184b100bba
Tobias Hagemann <tobias.hagemann@skymatic.de>
2021-11-29 18:03:13 +0100

    Reverted 6b69ced9a8093f8e6a636d93b7b9814dc0486618 but made sure to skip tests when iOS is higher than 15 until this is fixed

---
+++ CryptomatorCommonHostedTests/Keychain/VaultPasswordKeychainManagerTests.swift
@@ -0,0 +1,57 @@
+//
+//  VaultPasswordKeychainManagerTests.swift
+//  CryptomatorCommonHostedTests
+//
+//  Created by Philipp Schmid on 10.07.21.
+//  Copyright © 2021 Skymatic GmbH. All rights reserved.
+//
+
+import XCTest
+@testable import CryptomatorCommonCore
+
+class VaultPasswordKeychainManagerTests: XCTestCase {
+	func testSetAndRetrievePassword() throws {
+		if #available(iOS 15.0, *) {
+			throw XCTSkip("User presence keychain doesn't work in iOS 15 simulator (Radar 82890873)")
+		}
+		let passwordManager = VaultPasswordKeychainManager()
+		let password = "pw"
+		let vaultUID = UUID().uuidString
+		try passwordManager.setPassword(password, forVaultUID: vaultUID)
+		let fetchedPassword = try passwordManager.getPassword(forVaultUID: vaultUID)
+		XCTAssertEqual(password, fetchedPassword)
+		try passwordManager.removePassword(forVaultUID: vaultUID)
+	}
+
+	func testSetPasswordOverwritesExisting() throws {
+		if #available(iOS 15.0, *) {
+			throw XCTSkip("User presence keychain doesn't work in iOS 15 simulator (Radar 82890873)")
+		}
+		let passwordManager = VaultPasswordKeychainManager()
+		let oldPassword = "pw"
+		let vaultUID = UUID().uuidString
+		try passwordManager.setPassword(oldPassword, forVaultUID: vaultUID)
+		let newPassword = "newPW"
+		try passwordManager.setPassword(newPassword, forVaultUID: vaultUID)
+		let fetchedPassword = try passwordManager.getPassword(forVaultUID: vaultUID)
+		XCTAssertEqual(newPassword, fetchedPassword)
+		try passwordManager.removePassword(forVaultUID: vaultUID)
+	}
+
+	func testRemovePassword() throws {
+		if #available(iOS 15.0, *) {
+			throw XCTSkip("User presence keychain doesn't work in iOS 15 simulator (Radar 82890873)")
+		}
+		let passwordManager = VaultPasswordKeychainManager()
+		let password = "pw"
+		let vaultUID = UUID().uuidString
+		try passwordManager.setPassword(password, forVaultUID: vaultUID)
+		try passwordManager.removePassword(forVaultUID: vaultUID)
+		XCTAssertThrowsError(try passwordManager.getPassword(forVaultUID: vaultUID)) { error in
+			guard case VaultPasswordManagerError.passwordNotFound = error else {
+				XCTFail("Throws the wrong error: \(error)")
+				return
+			}
+		}
+	}
+}
