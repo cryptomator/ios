84257fdd7fe72564ed97717f74b253552f5cc0ca
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2021-12-08 12:49:24 +0100

    OnboardingViewController inherits now from BaseUITableViewController

--- Cryptomator/Onboarding/OnboardingViewController.swift
+++ Cryptomator/Onboarding/OnboardingViewController.swift
@@ -10,23 +10,14 @@ import CryptomatorCommonCore
 import Foundation
 import UIKit
 
-class OnboardingViewController: UITableViewController {
+class OnboardingViewController: BaseUITableViewController {
 	weak var coordinator: OnboardingCoordinator?
 
 	private let viewModel: OnboardingViewModel
 
 	init(viewModel: OnboardingViewModel) {
 		self.viewModel = viewModel
-		super.init(nibName: nil, bundle: nil)
-	}
-
-	@available(*, unavailable)
-	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
-
-	override func loadView() {
-		tableView = UITableView(frame: .zero, style: .grouped)
+		super.init()
 	}
 
 	override func viewDidLoad() {
