2354bb7e2345f8ad8ea98e1fda20148d08cd7fad
Tobias Hagemann <tobias.hagemann@skymatic.de>
2022-03-31 16:42:45 +0200

    Added localized error for WebDAVAuthenticatorError

---
+++ CryptomatorCommon/Sources/CryptomatorCommonCore/WebDAV/WebDAVAuthenticatorError+Localization.swift
@@ -0,0 +1,21 @@
+//
+//  WebDAVAuthenticatorError+Localization.swift
+//	CryptomatorCommonCore
+//
+//  Created by Tobias Hagemann on 31.03.22.
+//  Copyright © 2022 Skymatic GmbH. All rights reserved.
+//
+
+import CryptomatorCloudAccessCore
+import Foundation
+
+extension WebDAVAuthenticatorError: LocalizedError {
+	public var errorDescription: String? {
+		switch self {
+		case .unsupportedProcotol:
+			return LocalizedString.getValue("webDAVAuthenticator.error.unsupportedProtocol")
+		case .untrustedCertificate:
+			return LocalizedString.getValue("webDAVAuthenticator.error.untrustedCertificate")
+		}
+	}
+}
