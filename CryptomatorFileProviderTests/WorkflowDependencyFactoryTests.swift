40df202a788218c959eb96326ca947becba7289c
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2022-03-31 14:11:13 +0200

    Apply suggestions from code review
    Co-authored-by: Sebastian Stenzel <overheadhunter@users.noreply.github.com>

--- CryptomatorFileProviderTests/WorkflowDependencyFactoryTests.swift
+++ CryptomatorFileProviderTests/WorkflowDependencyFactoryTests.swift
@@ -45,12 +45,17 @@ class WorkflowDependencyFactoryTests: XCTestCase {
 	func testWriteTaskDependentsOnExistingWriteTask() throws {
 		let firstWriteTask = factory.createDependencies(for: CloudPath("/a/b"), lockType: .write)
 		let secondWriteTask = factory.createDependencies(for: CloudPath("/a"), lockType: .write)
+		// check if first tasks starts immediately (but isn't completed yet, blocking the second task)
 		wait(for: firstWriteTask.awaitPreconditions())
 		XCTAssertGetsNotExecuted(secondWriteTask.awaitPreconditions())
+		
+		// when the first task completes, check if the dependent second task starts (but doesn't finish yet)
 		firstWriteTask.workflowCompleted.fulfill(())
 
 		wait(for: secondWriteTask.awaitPreconditions())
 		XCTAssertGetsNotExecuted(secondWriteTask.workflowCompleted)
+		
+		// when the second task completes its completion promise is fulfilled
 		secondWriteTask.workflowCompleted.fulfill(())
 		wait(for: secondWriteTask.workflowCompleted)
 
@@ -80,13 +85,20 @@ class WorkflowDependencyFactoryTests: XCTestCase {
 	func testParallelReadTasks() throws {
 		let firstReadTask = factory.createDependencies(for: CloudPath("/a/b"), lockType: .read)
 		let secondReadTask = factory.createDependencies(for: CloudPath("/a"), lockType: .read)
+		// check if both tasks start immediately (but aren't completed yet)
 		wait(for: firstReadTask.awaitPreconditions())
 		wait(for: secondReadTask.awaitPreconditions())
 		XCTAssertGetsNotExecuted(firstReadTask.workflowCompleted)
 		XCTAssertGetsNotExecuted(secondReadTask.workflowCompleted)
+		
+		// when the first task completes its completion promise is fulfilled but the second task isn't
 		firstReadTask.workflowCompleted.fulfill(())
+		wait(for: firstReadTask.workflowCompleted)
 		XCTAssertGetsNotExecuted(secondReadTask.workflowCompleted)
+		
+		// when the second task completes its completion promise is fulfilled 
 		secondReadTask.workflowCompleted.fulfill(())
+		wait(for: secondReadTask.workflowCompleted)
 
 		let expectedFirstReadTaskLockPaths = [CloudPath("/"),
 		                                      CloudPath("/a"),
@@ -109,13 +121,17 @@ class WorkflowDependencyFactoryTests: XCTestCase {
 	func testWriteTaskDependentOnExistingReadTask() throws {
 		let dependenciesForReadTask = factory.createDependencies(for: CloudPath("/a/b"), lockType: .read)
 		let dependenciesForWriteTask = factory.createDependencies(for: CloudPath("/a"), lockType: .write)
+		// check if read tasks starts immediately (but isn't completed yet, blocking the write task)
 		wait(for: dependenciesForReadTask.awaitPreconditions())
 		XCTAssertGetsNotExecuted(dependenciesForWriteTask.awaitPreconditions())
 		XCTAssertGetsNotExecuted(dependenciesForReadTask.workflowCompleted)
 
+		// when the read task completes, check if the dependent write task starts (but doesn't finish yet)
 		dependenciesForReadTask.workflowCompleted.fulfill(())
 		wait(for: dependenciesForWriteTask.awaitPreconditions())
 		XCTAssertGetsNotExecuted(dependenciesForWriteTask.workflowCompleted)
+		
+		// when the write task completes its completion promise is fulfilled
 		dependenciesForWriteTask.workflowCompleted.fulfill(())
 		wait(for: dependenciesForWriteTask.workflowCompleted)
 
@@ -145,13 +161,17 @@ class WorkflowDependencyFactoryTests: XCTestCase {
 	func testReadTaskDependentOnExistingWriteTask() throws {
 		let dependenciesForWriteTask = factory.createDependencies(for: CloudPath("/a"), lockType: .write)
 		let dependenciesForReadTask = factory.createDependencies(for: CloudPath("/a/b"), lockType: .read)
+		// check if write tasks starts immediately (but isn't completed yet, blocking the read task)
 		wait(for: dependenciesForWriteTask.awaitPreconditions())
 		XCTAssertGetsNotExecuted(dependenciesForReadTask.awaitPreconditions())
 		XCTAssertGetsNotExecuted(dependenciesForWriteTask.workflowCompleted)
 
+		// when the write task completes, check if the dependent read task starts (but doesn't finish yet)
 		dependenciesForWriteTask.workflowCompleted.fulfill(())
 		wait(for: dependenciesForReadTask.awaitPreconditions())
 		XCTAssertGetsNotExecuted(dependenciesForReadTask.workflowCompleted)
+		
+		// when the read task completes its completion promise is fulfilled
 		dependenciesForReadTask.workflowCompleted.fulfill(())
 		wait(for: dependenciesForReadTask.workflowCompleted)
 
@@ -183,8 +203,11 @@ class WorkflowDependencyFactoryTests: XCTestCase {
 		let targetCloudPath = CloudPath("/a/c")
 
 		let dependenciesForMoveTask = factory.createDependencies(paths: [sourceCloudPath, targetCloudPath], lockType: .write)
+		// check if move task starts immediately (but doesn't complete yet)
 		wait(for: dependenciesForMoveTask.awaitPreconditions())
 		XCTAssertGetsNotExecuted(dependenciesForMoveTask.workflowCompleted)
+		
+		// when the move task completes its completion promise is fulfilled
 		dependenciesForMoveTask.workflowCompleted.fulfill(())
 		wait(for: dependenciesForMoveTask.workflowCompleted)
 
@@ -221,6 +244,7 @@ class WorkflowDependencyFactoryTests: XCTestCase {
 	func testErrorDoesNotPropagateBetweenDependentWorkflows() {
 		let dependenciesForReadTask = factory.createDependencies(for: CloudPath("/a/b"), lockType: .read)
 		let dependenciesForWriteTask = factory.createDependencies(for: CloudPath("/a"), lockType: .write)
+		// check if read task starts immediately (but doesn't complete yet, blocking the write task)
 		wait(for: dependenciesForReadTask.awaitPreconditions())
 		XCTAssertGetsNotExecuted(dependenciesForWriteTask.awaitPreconditions())
 		XCTAssertGetsNotExecuted(dependenciesForReadTask.workflowCompleted)
