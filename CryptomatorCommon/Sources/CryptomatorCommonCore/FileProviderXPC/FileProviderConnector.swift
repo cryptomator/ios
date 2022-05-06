afb31c6d2224ea0f9b49e142367c93befb612fa1
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2022-05-06 18:31:33 +0200

    Removed unused code from FileProviderConnector

--- CryptomatorCommon/Sources/CryptomatorCommonCore/FileProviderXPC/FileProviderConnector.swift
+++ CryptomatorCommon/Sources/CryptomatorCommonCore/FileProviderXPC/FileProviderConnector.swift
@@ -51,15 +51,6 @@ public class FileProviderXPCConnector: FileProviderConnector {
 
 	public static let shared = FileProviderXPCConnector()
 
-	public func getProxy<T>(serviceName: NSFileProviderServiceName, domainIdentifier: NSFileProviderDomainIdentifier) -> Promise<T> {
-		return NSFileProviderManager.getDomains().then { domains in
-			guard let domain = domains.first(where: { $0.identifier == domainIdentifier }) else {
-				throw FileProviderXPCConnectorError.domainNotFound
-			}
-			return self.getProxy(serviceName: serviceName, domain: domain)
-		}
-	}
-
 	public func getXPC<T>(serviceName: NSFileProviderServiceName, domain: NSFileProviderDomain?) -> Promise<XPC<T>> {
 		var url = NSFileProviderManager.default.documentStorageURL
 		if let domain = domain {
@@ -91,38 +82,6 @@ public class FileProviderXPCConnector: FileProviderConnector {
 			return XPC(proxy: proxy, doneHandler: {
 				connection.invalidate()
 			})
-		}
-	}
-
-	public func getProxy<T>(serviceName: NSFileProviderServiceName, domain: NSFileProviderDomain?) -> Promise<T> {
-		var url = NSFileProviderManager.default.documentStorageURL
-		if let domain = domain {
-			url.appendPathComponent(domain.pathRelativeToDocumentStorage)
-		}
-		return wrap { handler in
-			FileManager.default.getFileProviderServicesForItem(at: url, completionHandler: handler)
-		}.then { services -> Promise<NSXPCConnection?> in
-			if let desiredService = services?[serviceName] {
-				return desiredService.getFileProviderConnection()
-			} else {
-				return Promise(FileProviderXPCConnectorError.serviceNotSupported)
-			}
-		}.then { connection -> T in
-			guard let connection = connection else {
-				throw FileProviderXPCConnectorError.connectionIsNil
-			}
-			guard let type = T.self as AnyObject as? Protocol else {
-				throw FileProviderXPCConnectorError.typeMismatch
-			}
-			connection.remoteObjectInterface = NSXPCInterface(with: type)
-			connection.resume()
-			let rawProxy = connection.remoteObjectProxyWithErrorHandler { errorAccessingRemoteObject in
-				DDLogError("remoteObjectProxy failed with error: \(errorAccessingRemoteObject)")
-			}
-			guard let proxy = rawProxy as? T else {
-				throw FileProviderXPCConnectorError.rawProxyCastingFailed
-			}
-			return proxy
 		}
 	}
 }
