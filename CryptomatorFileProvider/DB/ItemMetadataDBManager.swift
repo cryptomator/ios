3653b04a811214f713cb349b3652dd8db84411ee
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2022-07-19 16:41:03 +0200

    Revert "Optimize `cacheMetadata(itemMetadataList:)` by using UPSERT"
    This reverts commit 406887b470651f67102e9ac4b7c8538755a7ac83.

--- CryptomatorFileProvider/DB/ItemMetadataDBManager.swift
+++ CryptomatorFileProvider/DB/ItemMetadataDBManager.swift
@@ -80,38 +80,19 @@ class ItemMetadataDBManager: ItemMetadataManager {
 		}
 	}
 
+	// TODO: Optimize Code and/or DB Scheme
 	func cacheMetadata(_ itemMetadataList: [ItemMetadata]) throws {
 		try database.write { db in
 			for metadata in itemMetadataList {
-				try db.execute(
-					sql: """
-					INSERT INTO \(ItemMetadata.databaseTableName)
-					(\(ItemMetadata.Columns.name), \(ItemMetadata.Columns.type), \(ItemMetadata.Columns.size), \(ItemMetadata.Columns.parentID), \(ItemMetadata.Columns.lastModifiedDate), \(ItemMetadata.Columns.statusCode), \(ItemMetadata.Columns.cloudPath), \(ItemMetadata.Columns.isPlaceholderItem), \(ItemMetadata.Columns.isMaybeOutdated), \(ItemMetadata.Columns.favoriteRank), \(ItemMetadata.Columns.tagData)) VALUES
-					(:name, :type, :size, :parentID, :lastModifiedDate, :statusCode, :cloudPath, :isPlaceholderItem, :isMaybeOutdated, :favoriteRank, :tagData)
-					ON CONFLICT (\(ItemMetadata.Columns.cloudPath))
-					DO UPDATE SET \(ItemMetadata.Columns.name) = excluded.\(ItemMetadata.Columns.name),
-					\(ItemMetadata.Columns.type) = excluded.\(ItemMetadata.Columns.type),
-					\(ItemMetadata.Columns.size) = excluded.\(ItemMetadata.Columns.size),
-					\(ItemMetadata.Columns.parentID) = excluded.\(ItemMetadata.Columns.parentID),
-					\(ItemMetadata.Columns.lastModifiedDate) = excluded.\(ItemMetadata.Columns.lastModifiedDate),
-					\(ItemMetadata.Columns.cloudPath) = excluded.\(ItemMetadata.Columns.cloudPath),
-					\(ItemMetadata.Columns.isPlaceholderItem) = excluded.\(ItemMetadata.Columns.isPlaceholderItem),
-					\(ItemMetadata.Columns.isMaybeOutdated) = excluded.\(ItemMetadata.Columns.isMaybeOutdated)
-					""",
-					arguments: ["name": metadata.name,
-					            "type": metadata.type,
-					            "size": metadata.size,
-					            "parentID": metadata.parentID,
-					            "lastModifiedDate": metadata.lastModifiedDate,
-					            "statusCode": metadata.statusCode,
-					            "cloudPath": metadata.cloudPath,
-					            "isPlaceholderItem": metadata.isPlaceholderItem,
-					            "isMaybeOutdated": metadata.isMaybeOutdated,
-					            "favoriteRank": metadata.favoriteRank,
-					            "tagData": metadata.tagData]
-				)
-				let metadataID = db.lastInsertedRowID
-				metadata.id = metadataID
+				if let cachedMetadata = try ItemMetadata.fetchOne(db, key: ["cloudPath": metadata.cloudPath]) {
+					metadata.id = cachedMetadata.id
+					metadata.statusCode = cachedMetadata.statusCode
+					metadata.tagData = cachedMetadata.tagData
+					metadata.favoriteRank = cachedMetadata.favoriteRank
+					try metadata.update(db)
+				} else {
+					try metadata.insert(db)
+				}
 			}
 		}
 	}
