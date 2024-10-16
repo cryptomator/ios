4cec92613559fb5a2cc14bebb09b74283c949bda
Majid Achhoud <majid.achhoud@gmail.com>
2024-10-16 18:16:40 +0200

    Added tinted and dark AppIcon (#385)
    Co-authored-by: Tobias Hagemann <tobias.hagemann@skymatic.de>

--- fastlane/scripts/create-app-icon.sh
+++ fastlane/scripts/create-app-icon.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 # Path is relative to the fastlane folder
-cp "${1}" ../SharedResources/Assets.xcassets/AppIcon.appiconset/Icon.png
+cp "${1}" ../SharedResources/Assets.xcassets/AppIcon.appiconset/LightIcon.png
+cp "${2}" ../SharedResources/Assets.xcassets/AppIcon.appiconset/DarkIcon.png
+cp "${3}" ../SharedResources/Assets.xcassets/AppIcon.appiconset/TintedIcon.png
